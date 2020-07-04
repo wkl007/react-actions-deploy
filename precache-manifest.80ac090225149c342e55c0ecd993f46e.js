c5e26b8bb16e26d7c2335a56ba6d0a58884fe102
wkl007 <499657357@qq.com>
2020-07-04 03:06:25 +0000

    Deploying to gh-pages from master @ 32d6239fcdda9892484af69220d7b14297cdb2e6 🚀

---
+++ precache-manifest.80ac090225149c342e55c0ecd993f46e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb77fed6dc8dbb7ba91da77eaebe2bf0",
+    "url": "/react-actions-deploy/index.html"
+  },
+  {
+    "revision": "c26b9246aabb329469d9",
+    "url": "/react-actions-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a343c32ad2cfd7f0927a",
+    "url": "/react-actions-deploy/static/js/2.4e603195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-actions-deploy/static/js/2.4e603195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26b9246aabb329469d9",
+    "url": "/react-actions-deploy/static/js/main.11ec1f20.chunk.js"
+  },
+  {
+    "revision": "85c2a91f3266cec64f14",
+    "url": "/react-actions-deploy/static/js/runtime-main.9f078f39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-actions-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-actions-deploy/precache-manifest.3621d653160ac8aa83e0787c4823f47e.js"
+  "/react-actions-deploy/precache-manifest.80ac090225149c342e55c0ecd993f46e.js"
 );
 
 self.addEventListener('message', (event) => {
